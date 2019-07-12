75ec2606ecb49f1d24bec6857c16b56de0fb78f1
Kuu Miyazaki <miyazaqui@gmail.com>
2019-07-12 10:07:26 +0900

    Prefix path-absolute urls with hostname

--- default.js
+++ default.js
@@ -23,14 +23,58 @@ function getUrlObj(url) {
   );
 }
 
-function defaultFunc(url) {
-  // Convert absolute urls into relative paths
-  const obj = getUrlObj(url);
-  if (!obj) {
-    // url is already a relative path
-    return url;
+function getUrlType(url) {
+  if (tryCatch(
+      () => {
+        url = new URL(url);
+        return true;
+      },
+      () => {
+        return false;
+      }
+    )) {
+    return 'absolute';
   }
-  return `/${obj.hostname}${obj.pathname}${obj.search}${obj.hash}`;
+
+  if (url.startsWith('//')) {
+    return 'scheme-relative';
+  }
+
+  if (url.startsWith('/')) {
+    return 'path-absolute';
+  }
+
+  return 'path-relative';
+}
+
+let baseHostName = '';
+
+function defaultFunc(url, saveAsBaseUrl) {
+  if (saveAsBaseUrl) {
+    baseHostName = '';
+  }
+
+  // Convert absolute urls into relative paths
+  let type = getUrlType(url);
+
+  if (type === 'scheme-relative') {
+    url = `http:${url}`;
+    type = 'absolute';
+  }
+
+  if (type === 'absolute') {
+    const obj = getUrlObj(url);
+    if (saveAsBaseUrl) {
+      baseHostName = obj.hostname;
+    }
+    return `/${obj.hostname}${obj.pathname}${obj.search}${obj.hash}`;
+  }
+
+  if (type === 'path-absolute' && baseHostName) {
+    return `/${baseHostName}${url}`;
+  }
+
+  return url;
 }
 
 module.exports = defaultFunc;
